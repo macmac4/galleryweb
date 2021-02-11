570c21c30fa884e29e0e18fe195bdac49d1ec959
Maciej B <maciej@galactica.pl>
2021-02-11 10:43:43 +0100

    add time ago

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,33 @@
 import { Link } from 'react-router-dom'
 
 const Album = ({ title, description, imageUrl, createdAt, id }) => {
+
+  const timeSince = (date) => {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
+
   return (
 
     <div className="col">
@@ -15,7 +42,7 @@ const Album = ({ title, description, imageUrl, createdAt, id }) => {
               <button type="button" className="btn btn-sm btn-outline-secondary"><Link to={`album/${id}`}>User Album</Link></button>
               {/* <button type="button" className="btn btn-sm btn-outline-secondary">Delete if user</button> */}
             </div>
-            <small className="text-muted">{createdAt.toDate().toDateString()}</small>
+            <small className="text-muted">{timeSince(createdAt.seconds * 1000)} ago</small>
           </div>
         </div>
       </div>
